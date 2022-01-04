1c041f6a7e021c2d8e010a044e0d9a9f8b29a89d
Guilherme <guilherme.gke@gmail.com>
2022-01-04 13:45:45 -0300

    Criando o primeiro Componente

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './Components/App';
+
 ReactDOM.render(
-  <h1>
-    <span>Hello World!</span>
-  </h1>,
+  <App />,
   document.getElementById('root'),
 );
