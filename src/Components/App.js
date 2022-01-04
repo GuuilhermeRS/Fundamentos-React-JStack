19db2b563ca645d4f31b9feabe2194865cb7cb19
Guilherme <guilherme.gke@gmail.com>
2022-01-04 17:44:36 -0300

    React Props - Prop Children, Tipando Props, Props Default

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,40 @@
 import React from 'react';
 
+import Header from './Header';
+import Post from './Post';
+
 function App() {
-  return <h1>Componente App</h1>;
+  return (
+    <>
+      <Header>
+        <h2>Posts da semana</h2>
+      </Header>
+
+      <hr/>
+      
+      <Post
+        likes = {20}
+        post = {{
+          title: 'Titulo da notícia 01',
+          subtitle: 'Subtítulo da notícia 01'
+        }}
+      />
+      <Post 
+        likes = {10}
+        post = {{
+          title: 'Titulo da notícia 02',
+          subtitle: 'Subtítulo da notícia 02'
+        }}
+      />
+      <Post 
+        likes = {15}
+        post = {{
+          title: 'Titulo da notícia 03',
+          subtitle: 'Subtítulo da notícia 03'
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
