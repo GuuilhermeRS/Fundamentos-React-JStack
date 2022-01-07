55ee069494de80dfccd59fc511fa8b25a07cdc08
Guilherme <guilherme.gke@gmail.com>
2022-01-07 08:11:54 -0300

    Renderização condicional

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,9 +6,9 @@ import Post from './Post';
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20 },
-    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 50 },
-    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 10 },
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false },
+    { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 50, read: true },
+    { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 10, read: false },
   ]);
 
   function handleRefresh() {
@@ -43,12 +43,13 @@ function App() {
       {posts.map(post => (
         <Post 
           key={post.id}
-          likes={post.likes}
           onRemove={handleRemovePost}
           post={{
             id: post.id,
+            read: post.read,
             title: post.title,
             subtitle: post.subtitle,
+            likes: post.likes
           }}
         />
       ))}    
