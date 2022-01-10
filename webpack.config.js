762bd398b63d7e8fe0c24ab2b46be5772231895b
Guilherme <guilherme.gke@gmail.com>
2022-01-10 09:05:13 -0300

    Utilizando CSS Modules

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,12 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            }
+          }
         ]
       }
     ]
