0c6012897cc65a2a38995651a870707af76cfe6f
GuilhermeRS <guilherme.gke@gmail.com>
2022-04-06 08:58:53 -0300

    Página 404, e Switch para acessar apenas uma rota por vez

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 import Home from './pages/Home';
 import PostsList from './components/PostsList';
+import NotFound from './pages/NotFound';
 
 export default function Routes() {
   return (
-    <>
+    <Switch>
+      {/* Switch faz com que so seja procurado uma rota em nossa aplicação*/}
       <Route exact path="/" component={Home} />
       <Route path="/posts" component={PostsList} />
-    </>
+      <Route component={NotFound} />
+    </Switch>
   );
 }
