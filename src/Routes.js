a079798713c66de7d3a956f44845392842e6519c
GuilhermeRS <guilherme.gke@gmail.com>
2022-04-06 08:50:51 -0300

    Prop exact e component Link

--- src/Routes.js
+++ src/Routes.js
@@ -6,9 +6,9 @@ import PostsList from './components/PostsList';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Route path="" component={Home} />
+    <>
+      <Route exact path="/" component={Home} />
       <Route path="/posts" component={PostsList} />
-    </BrowserRouter>
+    </>
   );
 }
