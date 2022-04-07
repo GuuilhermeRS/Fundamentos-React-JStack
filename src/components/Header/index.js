85873da618292e76675f8c376fcea05f2ee22262
GuilhermeRS <guilherme.gke@gmail.com>
2022-04-07 09:32:48 -0300

    Navegação programática

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,17 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { Container } from './styles';
 
 import { ThemeContext } from '../../ThemeContext';
 
 export default function Header() {
-
   const { theme, onToggleTheme } = useContext(ThemeContext);
+  const history = useHistory();
+
+  function handleNavigate() {
+    history.push('/posts');
+  }
 
   return (
     <Container>
@@ -17,6 +22,7 @@ export default function Header() {
       >
         {theme === 'dark' ? '🌞' : '🌚'}
       </button>
+      <button onClick={handleNavigate} style={{ color: '#fff' }}>Posts</button>
     </Container>
   );
 }
