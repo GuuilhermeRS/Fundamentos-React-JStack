205b6d955cc29239d335aa0883b16946162e9793
GuilhermeRS <guilherme.gke@gmail.com>
2022-04-07 09:23:26 -0300

    Recebendo parâmetros e Query Params nas Rotas

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,15 @@ import Home from './pages/Home';
 import PostsList from './components/PostsList';
 import NotFound from './pages/NotFound';
 
+import Post from './pages/Post';
+
 export default function Routes() {
   return (
     <Switch>
       {/* Switch faz com que so seja procurado uma rota em nossa aplicação*/}
       <Route exact path="/" component={Home} />
-      <Route path="/posts" component={PostsList} />
+      <Route exact path="/posts" component={PostsList} />
+      <Route path="/posts/:id" component={Post} />
       <Route component={NotFound} />
     </Switch>
   );
